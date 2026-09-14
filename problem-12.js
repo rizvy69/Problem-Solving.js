5b2f3e6ac6f04dc18cb162292b7df726fdc40138
Redowan Ahamed Rizvy <ridowanahamed69@gmail.com>
2026-09-14 23:46:29 +0600

    third commit

---
+++ problem-12.js
@@ -0,0 +1,10 @@
+// Problem - 12: Filter Numbers Greater than a value
+
+let arr = [10,20,30,40,50,60,70,80,90,100]
+let value = 50;
+
+let filterArray = [];
+
+for(let i = 0; i < arr.length;i++){
+    
+}
